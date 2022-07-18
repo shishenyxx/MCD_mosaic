1affd972ccf88fc80a0d7387e9c157dd7912f2b5
Xiaoxu Yang <yangxx@mail.cbi.pku.edu.cn>
2022-07-18 15:30:14 -0700

    Update Permutation.sh

--- Stastical_analysis/Gene_permutation/Permutation.sh
+++ Stastical_analysis/Gene_permutation/Permutation.sh
@@ -7,10 +7,11 @@ my_join.pl -F 1 -f 1 -a Human_full_gene_list.txt -b FCD_list -m|wc -l
 for i in {1..10000};do my_join.pl -F 1 -f 1 -a <(shuf Human_full_gene_list.txt|head -n 1686) -b FCD_list -m|wc -l>>10k_permutations;done
 
 
-##permute the number of genes detected in HME, TSC, and HME to see enrichments compared with control
+## permute the number of genes detected in HME, TSC, and HME to see enrichments compared with control
 
 for i in {1..10000};do my_join.pl -F 1 -f 1 -a <(shuf shuf Human_full_gene_list.txt|head -n 1735) -b FCD_list -m|wc -l>>10k_permutations_HME;done
 
 for i in {1..10000};do my_join.pl -F 1 -f 1 -a <(shuf shuf Human_full_gene_list.txt|head -n 1017) -b FCD_list -m|wc -l>>10k_permutations_TSC;done
 
 for i in {1..10000};do my_join.pl -F 1 -f 1 -a <(shuf shuf Human_full_gene_list.txt|head -n 981) -b FCD_list -m|wc -l>>10k_permutations_FCD;done
+
